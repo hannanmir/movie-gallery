4a64e1e00fd1ec981b925e3c1964d2600752f5a2
Hannan Mir <hannanmir2@gmail.com>
2020-08-30 14:54:48 -0500

    some styling for movie posters

--- src/components/Details/Details.jsx
+++ src/components/Details/Details.jsx
@@ -5,8 +5,11 @@ import { withRouter } from 'react-router-dom'
 class Details extends Component{
     render() {
         return(
-            <>
-            </>
+            <div className="detail">
+                <h2>{this.props.reduxState.detailsReducer.title}</h2>
+                <img src={this.props.reduxState.detailsReducer.poster} alt={this.props.reduxState.detailsReducer.title} />
+                <p>{this.props.reduxState.detailsReducer.description}</p>
+            </div>
         )
     }
 }
--- src/components/MovieListItem/MovieListItem.jsx
+++ src/components/MovieListItem/MovieListItem.jsx
@@ -6,6 +6,7 @@ class MovieListItem extends Component{
     clickMovie = () => {
         console.log('Clicked!');
         this.props.dispatch({ type: 'WHICH_MOVIE', payload: this.props.movie })
+        this.props.history.push('/details')
         console.log(this.props.movie);
     }
 
