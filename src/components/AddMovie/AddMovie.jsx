c928c32e561ce3bdbcfda5a2bbf214baa06c06b3
Hannan Mir <hannanmir2@gmail.com>
2020-08-30 20:02:14 -0500

    formatting for input

--- src/components/AddMovie/AddMovie.jsx
+++ src/components/AddMovie/AddMovie.jsx
@@ -7,8 +7,6 @@ import TextField from '@material-ui/core/TextField';
 import Select from '@material-ui/core/Select';
 import MenuItem from '@material-ui/core/MenuItem';
 import FormControl from '@material-ui/core/FormControl';
-import InputLabel from '@material-ui/core/InputLabel';
-
 
 class AddMovie extends Component{
     // intitial states are empty/ 0 used for input validation
@@ -20,7 +18,6 @@ class AddMovie extends Component{
             genre_id: 0,
         }
     }
-
 
     // allows various keys of the newMovie object to be changed with one function
     handleChangeFor = (event, propertyToChange) => {
@@ -51,7 +48,7 @@ class AddMovie extends Component{
         }
     }
 
-    // cancles the add movie process, sets initial state, and routes back to home
+    // cancels the add movie process, sets initial state, and routes back to home
     cancelMovie = () => {
         this.setState({
             newMovie: {
@@ -67,39 +64,39 @@ class AddMovie extends Component{
     // Inputs for title, description, poster url, and genre
     render() {
         return(
-            <>
-                <FormControl >
+            <div>
+                <div className="add">
                     <TextField id="standard-basic" label="Title" value={this.state.newMovie.title} onChange = {(event) => this.handleChangeFor(event, 'title')}/>
                     <TextField id="standard-basic" label="Description" value={this.state.newMovie.description} onChange = {(event) => this.handleChangeFor(event, 'description')}/>
                     <TextField id="standard-basic" label="Poster URL" value={this.state.newMovie.poster} onChange = {(event) => this.handleChangeFor(event, 'poster')}/>
-                    <Select id="genre" value={this.state.newMovie.genre_id} onChange={(value) => this.handleChangeFor(value, 'genre_id')}>
-                        <MenuItem value={0}>Choose Genre</MenuItem>
-                        <MenuItem value={1}>Adventure</MenuItem>
-                        <MenuItem value={2}>Animated</MenuItem>
-                        <MenuItem value={3}>Biographical</MenuItem>
-                        <MenuItem value={4}>Comedy</MenuItem>
-                        <MenuItem value={5}>Disaster</MenuItem>
-                        <MenuItem value={6}>Drama</MenuItem>
-                        <MenuItem value={7}>Epic</MenuItem>
-                        <MenuItem value={8}>Fantasy</MenuItem>
-                        <MenuItem value={9}>Musical</MenuItem>
-                        <MenuItem value={10}>Romantic</MenuItem>
-                        <MenuItem value={11}>Science Fiction</MenuItem>
-                        <MenuItem value={12}>Space-Opera</MenuItem>
-                        <MenuItem value={13}>Superhero</MenuItem>
-                        <MenuItem value={14}>Psychological Thriller</MenuItem>
-                        <MenuItem value={15}>Spy</MenuItem>
-                    </Select>
+                        <Select id="genre" value={this.state.newMovie.genre_id} onChange={(value) => this.handleChangeFor(value, 'genre_id')}>
+                            <MenuItem value={0}>Choose Genre</MenuItem>
+                            <MenuItem value={1}>Adventure</MenuItem>
+                            <MenuItem value={2}>Animated</MenuItem>
+                            <MenuItem value={3}>Biographical</MenuItem>
+                            <MenuItem value={4}>Comedy</MenuItem>
+                            <MenuItem value={5}>Disaster</MenuItem>
+                            <MenuItem value={6}>Drama</MenuItem>
+                            <MenuItem value={7}>Epic</MenuItem>
+                            <MenuItem value={8}>Fantasy</MenuItem>
+                            <MenuItem value={9}>Musical</MenuItem>
+                            <MenuItem value={10}>Romantic</MenuItem>
+                            <MenuItem value={11}>Science Fiction</MenuItem>
+                            <MenuItem value={12}>Space-Opera</MenuItem>
+                            <MenuItem value={13}>Superhero</MenuItem>
+                            <MenuItem value={14}>Psychological Thriller</MenuItem>
+                            <MenuItem value={15}>Spy</MenuItem>
+                        </Select>
                     <Button variant="contained" size="small" color="secondary" onClick= {() => this.cancelMovie()}>Cancel</Button>
                     <Button variant="contained" size="small" color="primary" onClick={ () => this.addMovie()}>Save Movie</Button>
-                </FormControl>
+                </div>
                 {/* shows the user the new movie info as they are entering it */}
-                <div className="detail">
+                <div className="detail2">
                     <h2>{this.state.newMovie.title}</h2>
                     <img src={this.state.newMovie.poster} alt={this.state.newMovie.title} />
                     <p>{this.state.newMovie.description}</p>
                 </div>
-            </>
+            </div>
         )
     }
 }
